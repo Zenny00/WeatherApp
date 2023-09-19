0fa44d0bbd233692d63fa2406fe4bd2bd8aee84f
Zenny <joshuacomfort@gmail.com>
2023-09-19 08:55:09 -0400

    Added weekdays

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,17 +1,25 @@
 $(document).ready(function () {
   navigator.geolocation.getCurrentPosition((location) => {
-    const URL = "https://api.open-meteo.com/v1/forecast?latitude=" + location.coords.latitude + "&longitude=" + location.coords.longitude + "&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,precipitation,rain,showers,snowfall,visibility,windspeed_10m&timezone=America%2FNew_York"
+    const URL = "https://api.open-meteo.com/v1/forecast?latitude=" + location.coords.latitude + "&longitude=" + location.coords.longitude + "&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,snowfall_sum,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York";
     $.ajax({ 
       url: URL,
       type: "GET",
       success: function(result) {
         console.log(result);
         var div = document.getElementById("weather_info");
-        for (var i = 0; i < result.hourly.temperature_2m.length; i++) {
-         result.hourly.temperature_2m[i] = (((9/5) * result.hourly.temperature_2m[i]) + 32).toFixed(0);
+        var days = document.getElementById("day_info");
+        for (var i = 0; i < result.daily.temperature_2m_max.length; i++) {
+         result.daily.temperature_2m_max[i] = (result.daily.temperature_2m_max[i]).toFixed(0).toString() + "&deg;F";
         }
-        
-        div.innerHTML = result.hourly.temperature_2m;
+         
+        div.innerHTML = result.daily.temperature_2m_max;
+        const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        const current_day = new Date().getDay();
+        const weekdays = [];
+        for (var i = 0; i < weekday.length; i++) {
+          weekdays.push(weekday[(current_day + i) % 7]);
+        }
+        days.innerHTML = weekdays;
       },
       error: function(error) {
         console.log(`Error ${error}`)
