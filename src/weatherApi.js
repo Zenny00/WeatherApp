631a96c223b3e6ebf1e7b516dff8dd2100265510
Zenny <joshuacomfort@gmail.com>
2023-09-18 22:41:37 -0400

    Added geolocation to API call

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
-  const URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,precipitation,rain,showers,snowfall,visibility,windspeed_10m&timezone=America%2FNew_York'
-  $.ajax({ 
-    url: URL,
-    type: "GET",
-    success: function(result) {
-      console.log(result)
-    },
-    error: function(error) {
-      console.log(`Error ${error}`)
-    }
+$(document).ready(function () {
+  navigator.geolocation.getCurrentPosition((location) => {
+    const URL = "https://api.open-meteo.com/v1/forecast?latitude=" + location.coords.latitude + "&longitude=" + location.coords.longitude + "&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,precipitation,rain,showers,snowfall,visibility,windspeed_10m&timezone=America%2FNew_York"
+    $.ajax({ 
+      url: URL,
+      type: "GET",
+      success: function(result) {
+        console.log(result)
+      },
+      error: function(error) {
+        console.log(`Error ${error}`)
+      }
+    })
   })
 })
