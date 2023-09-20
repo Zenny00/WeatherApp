4fb09fbf1e215aecb40aad804e91a3ac67876ea7
Zenny <joshuacomfort@gmail.com>
2023-09-20 08:34:10 -0400

    Added date

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,8 +1,9 @@
-function createDiv(parentDiv, temperature_max, temperature_min, day) {
+function createDiv(parentDiv, temperature_max, temperature_min, day, date) {
   document.getElementById(parentDiv).innerHTML += '<div class="grid grid-cols-1 gap-4 place-items-stretch">'
     + '<div style="text-align: center;" class="text-slate-50 text-2xl">' + temperature_max + '</div>'
     + '<div style="text-align: center;" class="text-slate-600 text-m">' + temperature_min + '</div>'
-    + '<div style="text-align: center;" class="text-slate-100 text-xl">' + day + '</div></div>';
+    + '<div style="text-align: center;" class="text-slate-100 text-xl">' + day + '</div>'
+    + '<div style="text-align: center;" class="text-slate-600 text-l">' + date + '</div></div>';
 }
 
 $(document).ready(function () {
@@ -16,13 +17,14 @@ $(document).ready(function () {
         for (var i = 0; i < result.daily.temperature_2m_max.length; i++) {
          result.daily.temperature_2m_max[i] = (result.daily.temperature_2m_max[i]).toFixed(0).toString() + "&deg;F";
          result.daily.temperature_2m_min[i] = (result.daily.temperature_2m_min[i]).toFixed(0).toString() + "&deg;F";
+         result.daily.time[i] = result.daily.time[i].substring(5); 
         }
          
         const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         const current_day = new Date().getDay();
         const weekdays = [];
         for (var i = 0; i < weekday.length; i++) {
-          createDiv("forecast_box", result.daily.temperature_2m_max[i], result.daily.temperature_2m_min[i], weekday[(current_day + i) % 7]);
+          createDiv("forecast_box", result.daily.temperature_2m_max[i], result.daily.temperature_2m_min[i], weekday[(current_day + i) % 7], result.daily.time[i]);
         }
       },
       error: function(error) {
