1068ce71c81ff2dda9e8cb4d27d48674c7e64369
Zenny <joshuacomfort@gmail.com>
2023-09-19 20:05:10 -0400

    Updated content to fill middle of page

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,3 +1,8 @@
+function createDiv(parentDiv, temperature, day) {
+  document.getElementById(parentDiv).innerHTML += '<div class="grid grid-cols-1 gap-4">'
+    + '<div>' + temperature + '</div><div>' + day + '</div></div>';
+}
+
 $(document).ready(function () {
   navigator.geolocation.getCurrentPosition((location) => {
     const URL = "https://api.open-meteo.com/v1/forecast?latitude=" + location.coords.latitude + "&longitude=" + location.coords.longitude + "&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,snowfall_sum,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York";
@@ -6,20 +11,16 @@ $(document).ready(function () {
       type: "GET",
       success: function(result) {
         console.log(result);
-        var div = document.getElementById("weather_info");
-        var days = document.getElementById("day_info");
         for (var i = 0; i < result.daily.temperature_2m_max.length; i++) {
          result.daily.temperature_2m_max[i] = (result.daily.temperature_2m_max[i]).toFixed(0).toString() + "&deg;F";
         }
          
-        div.innerHTML = result.daily.temperature_2m_max;
         const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         const current_day = new Date().getDay();
         const weekdays = [];
         for (var i = 0; i < weekday.length; i++) {
-          weekdays.push(weekday[(current_day + i) % 7]);
+          createDiv("forecast_box", result.daily.temperature_2m_max[i], weekday[(current_day + i) % 7]);
         }
-        days.innerHTML = weekdays;
       },
       error: function(error) {
         console.log(`Error ${error}`)
